84af14fc1bba25d2d0a7b63f5af5a98ab8340ae5
Diego Alejandro Vera Trujillo <diegoverat1796@gmail.com>
2023-08-25 10:29:27 -0500

    ADD: session services

---
+++ scripts/services/session-services.js
@@ -0,0 +1,17 @@
+import { tokenKey } from "../config.js";
+import apiFetch from "./api-fetch.js";
+
+async function login(credentials = { email, password }) {
+  const { token, ...user } = await apiFetch("/login", { body: credentials });
+  console.log(user);
+  sessionStorage.setItem(tokenKey, token);
+  return user;
+}
+
+async function logout() {
+  const response = await apiFetch("/logout", { method: "DELETE" });
+  sessionStorage.removeItem(tokenKey);
+  return response;
+}
+
+export { login, logout };
